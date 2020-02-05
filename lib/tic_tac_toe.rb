25bfcd6d0b42f34890506ef3b38c5d3a453e8bc6
k3ntako <kentarokaneki@gmail.com>
2020-02-05 16:55:29 -0500

    Use CLI.putString for TicTacToe.printWelcome
    Interaction with the console with be done through CLI class

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,8 @@
+require_relative "./cli"
+
 class TicTacToe
   def printWelcome
-    puts "Welcome to a game of Tic-Tac-Toe!"
+    welcome_message = "Welcome to a game of Tic-Tac-Toe!"
+    CLI.putString welcome_message
   end
 end
