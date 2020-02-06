55b1a9f9081f5c813edf94665fd653311f475948
k3ntako <kentarokaneki@gmail.com>
2020-02-06 09:50:38 -0500

    Print empty board
    In the future, print_board will take a board print that to the console.
    This functionality is specific to the CLI, and thus it is in the CLI
    class.

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,4 +2,12 @@ class CLI
   def self.putString(text)
     puts text
   end
+
+  def self.put_board
+    puts "   |   |   \n" +
+    "-----------\n" +
+    "   |   |   \n" +
+    "-----------\n" +
+    "   |   |   \n"
+  end
 end
--- spec/lib/cli_spec.rb
+++ spec/lib/cli_spec.rb
@@ -1,10 +1,23 @@
+require_relative "../../lib/board"
 require_relative "../../lib/cli"
 
 RSpec.describe "CLI" do
-  context "when string is passed into CLI.puts" do
+  context "when string is passed into CLI.putString" do
     it "should print the string to the console" do
       text = "This should be printed to the console"
       expect { CLI.putString text }.to output(text + "\n").to_stdout
+    end
+  end
+
+  context "when CLI.put_board is called" do
+    it "should print an empty board to the console" do
+      expected_output = "   |   |   \n" +
+      "-----------\n" +
+      "   |   |   \n" +
+      "-----------\n" +
+      "   |   |   \n"
+
+      expect { CLI.put_board }.to output(expected_output).to_stdout
     end
   end
 end
