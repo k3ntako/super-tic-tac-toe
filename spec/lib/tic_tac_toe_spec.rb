2f25f8e6f06cb9b24cc8ca1d9e88b8190f1101a4
k3ntako <kentarokaneki@gmail.com>
2020-02-06 15:09:10 -0500

    Fix test
    Quote type matters with new line, \n.

--- spec/lib/tic_tac_toe_spec.rb
+++ spec/lib/tic_tac_toe_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'TicTacToe' do
   context 'when the user starts the game' do
     it 'should print a welcome message' do
       tic_tac_toe = TicTacToe.new
-      expected_message = 'Welcome to a game of Tic-Tac-Toe!\n'
+      expected_message = "Welcome to a game of Tic-Tac-Toe!\n"
 
       expect { tic_tac_toe.print_welcome }.to output(expected_message).to_stdout
     end
