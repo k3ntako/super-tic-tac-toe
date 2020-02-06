51050744b8afc0ae30c3061cb4c798899e96f37b
k3ntako <kentarokaneki@gmail.com>
2020-02-06 12:46:15 -0500

    Add test double
    Instead of checking the final outcome, a unit test should just test the
    method of interest.

--- spec/lib/tic_tac_toe_spec.rb
+++ spec/lib/tic_tac_toe_spec.rb
@@ -3,8 +3,11 @@ require_relative "../../lib/tic_tac_toe"
 RSpec.describe "TicTacToe" do
   context "when the user starts the game" do
     it "should print a welcome message" do
-      tic_tac_toe = TicTacToe.new;
-      expect { tic_tac_toe.print_welcome }.to output("Welcome to a game of Tic-Tac-Toe!\n").to_stdout
+      cli = CLI.new
+      expect(cli).to receive(:put_string).with("Welcome to a game of Tic-Tac-Toe!").once
+
+      tic_tac_toe = TicTacToe.new(cli)
+      tic_tac_toe.print_welcome
     end
   end
 end
