4f3c51560fea53ed74562fe0ff71f4e8db676b84
k3ntako <kentarokaneki@gmail.com>
2020-02-17 16:10:18 -0500

    Clarify comments

--- lib/board.rb
+++ lib/board.rb
@@ -29,16 +29,16 @@ class Board
   end
 
   def move_valid?(position)
-    # Accepts an integer or string
+    # Requires an integer or string
     return false if (!position.is_a? Integer) && (!position.is_a? String)
 
     # Strings will be converted to integer using Integer
-    # Valid strings: "1", "  1 ", " 2 \n", " 2 \r\n"
-    # Invalid strings: "", "aa", "a1", "1a", "1 2", "1.1"
+    # Valid strings: '1', '  1 ', ' 2 \n', ' 2 \r\n'
+    # Invalid strings: '', 'aa', 'a1', '1a', '1 2', '1.1'
     begin
       position = Integer position
     rescue ArgumentError
-      return false
+      return false # Failed to parse String into Integer
     end
 
     return false if position < 1 || position > 9
@@ -46,7 +46,7 @@ class Board
     row = position_to_row position
     col = position_to_col position
 
-    # Check if specified location is already occupied
+    # Check if specified square on board is already occupied
     @board[row][col].nil?
   end
 end
