eb215dc2352e27b18aae249715f9452780afa2cc
k3ntako <kentarokaneki@gmail.com>
2020-02-18 14:06:28 -0500

    Make UserInputValidator class
    Board should not be the one judging if an input is valid.

---
+++ lib/user_input_validator.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# UserInputValidator
+class UserInputValidator
+  def move_valid_integer?(input_string)
+    # Requires an integer or string
+    if (!input_string.is_a? Integer) && (!input_string.is_a? String)
+      return false
+    end
+
+    # Strings will be converted to integer using Integer
+    # Valid strings: '1', '  1 ', ' 2 \n', ' 2 \r\n'
+    # Invalid strings: '', 'aa', 'a1', '1a', '1 2', '1.1'
+    begin
+      input_string = Integer input_string
+    rescue ArgumentError
+      return false # Failed to parse String into Integer
+    end
+
+    return false if input_string < 1 || input_string > 9
+
+    true
+  end
+
+  def move_on_empty_square?(board, position)
+    board.occupied? position
+  end
+end
---
+++ spec/lib/user_input_validator_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/user_input_validator'
+
+RSpec.describe UserInputValidator do
+  let(:user_input_validator) { UserInputValidator.new }
+  let(:board) { Board.new }
+
+  context 'when move_valid_integer? is called with a string' do
+    it 'should return true given a valid position' do
+      is_valid = user_input_validator.move_valid_integer? 1
+
+      expect(is_valid).to be true
+    end
+
+    it 'should return true given a valid position as a string' do
+      is_valid = user_input_validator.move_valid_integer? '2'
+
+      expect(is_valid).to be true
+    end
+
+    it 'should return false if value is less than 1' do
+      is_valid = user_input_validator.move_valid_integer? 0
+
+      expect(is_valid).to be false
+    end
+
+    it 'should return false if value is greater than 0' do
+      is_valid = user_input_validator.move_valid_integer? 10
+
+      expect(is_valid).to be false
+    end
+
+    it 'should return false if value is a string with non-integers' do
+      is_valid = user_input_validator.move_valid_integer? '1one'
+
+      expect(is_valid).to be false
+    end
+
+    it 'should return false if value is a float' do
+      is_valid = user_input_validator.move_valid_integer? 1.0
+
+      expect(is_valid).to be false
+    end
+  end
+
+  context 'when move_on_empty_square? is called with a valid string' do
+    it 'should return true if specified square is empty' do
+      is_valid = user_input_validator.move_on_empty_square?(board, 1)
+
+      expect(is_valid).to be true
+    end
+
+    it 'should return false if position already has a piece' do
+      board_played_at_one = [
+        ['X', nil, nil],
+        [nil, nil, nil],
+        [nil, nil, nil]
+      ]
+
+      board.instance_variable_set(:@board, board_played_at_one)
+
+      is_valid = user_input_validator.move_on_empty_square?(board, 1)
+
+      expect(is_valid).to be false
+    end
+  end
+end
