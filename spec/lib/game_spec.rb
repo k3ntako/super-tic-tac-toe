3507a4e8c252e8d2b9f53f4191c0c0caa0e30ce5
k3ntako <kentarokaneki@gmail.com>
2020-02-24 15:10:12 -0500

    Break up Game tests

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -12,22 +12,40 @@ RSpec.describe Game do
   end
 
   describe 'start' do
+    it 'should alternate players' do
+      player_one = game.instance_variable_get(:@current_player)
+      player_two = game.instance_variable_get(:@previous_player)
+
+      allow(game).to receive(:display_board)
+      allow(game).to receive(:display_move_instruction)
+
+      expect(player_one).to receive(:get_move).once.ordered.and_return 1
+      expect(player_two).to receive(:get_move).once.ordered.and_return 2
+      expect(player_one).to receive(:get_move).once.ordered.and_return 4
+      expect(player_two).to receive(:get_move).once.ordered.and_return 5
+      expect(player_one).to receive(:get_move).once.ordered.and_return 7
+
+      allow(game).to receive(:exit_game_with_winner)
+
+      game.start
+    end
+
     context 'when a game ends in a tie' do
       it 'should call exit_game_with_tie' do
+        board = Board.new
+        played_board_state = [
+          ['O', 'X', 'O'],
+          ['X', 'X', 'O'],
+          ['X', 'O', nil]
+        ]
+
+        board.instance_variable_set(:@board, played_board_state)
+        game.instance_variable_set(:@board, board)
+
         player_one = game.instance_variable_get(:@current_player)
-        player_two = game.instance_variable_get(:@previous_player)
 
-        expect(game).to receive(:display_board).exactly(10).times
-        expect(game).to receive(:display_move_instruction).exactly(9).times
-
-        expect(player_one).to receive(:get_move).once.ordered.and_return 5
-        expect(player_two).to receive(:get_move).once.ordered.and_return 1
-        expect(player_one).to receive(:get_move).once.ordered.and_return 7
-        expect(player_two).to receive(:get_move).once.ordered.and_return 3
-        expect(player_one).to receive(:get_move).once.ordered.and_return 2
-        expect(player_two).to receive(:get_move).once.ordered.and_return 8
-        expect(player_one).to receive(:get_move).once.ordered.and_return 4
-        expect(player_two).to receive(:get_move).once.ordered.and_return 6
+        expect(game).to receive(:display_board).twice
+        expect(game).to receive(:display_move_instruction).once
         expect(player_one).to receive(:get_move).once.ordered.and_return 9
 
         expect(game).to receive(:exit_game_with_tie).once.ordered
@@ -38,17 +56,21 @@ RSpec.describe Game do
 
     context 'when a user wins' do
       it 'should call exit_game_with_winner' do
+        board = Board.new
+        played_board_state = [
+          ['O', 'X', nil],
+          [nil, 'X', 'O'],
+          ['X', 'O', nil]
+        ]
+
+        board.instance_variable_set(:@board, played_board_state)
+        game.instance_variable_set(:@board, board)
+
         player_one = game.instance_variable_get(:@current_player)
-        player_two = game.instance_variable_get(:@previous_player)
 
-        expect(game).to receive(:display_board).exactly(6).times
-        expect(game).to receive(:display_move_instruction).exactly(5).times
-
-        expect(player_one).to receive(:get_move).once.ordered.and_return 5
-        expect(player_two).to receive(:get_move).once.ordered.and_return 1
-        expect(player_one).to receive(:get_move).once.ordered.and_return 2
-        expect(player_two).to receive(:get_move).once.ordered.and_return 1
-        expect(player_one).to receive(:get_move).once.ordered.and_return 8
+        expect(game).to receive(:display_board).twice
+        expect(game).to receive(:display_move_instruction).once
+        expect(player_one).to receive(:get_move).once.ordered.and_return 3
 
         expect(game).to receive(:exit_game_with_winner).once.ordered.with player_one
 
