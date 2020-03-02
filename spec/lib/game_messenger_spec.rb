473199d4fd0297ee01eb39540a25c76e7223abea
Kentaro Kaneki <kentarokaneki@gmail.com>
2020-03-02 15:49:56 -0500

    Refactor game_messenger test

--- spec/lib/game_messenger_spec.rb
+++ spec/lib/game_messenger_spec.rb
@@ -1,27 +1,30 @@
 require_relative '../../lib/player'
 require_relative '../../lib/game_messenger'
 
-RSpec.describe GameMessenger do
-  let(:cli) { CLI.new }
-  let(:user_interface) { UserInterface.new(cli) }
-  let(:game_messenger) { GameMessenger.new(user_interface: user_interface) }
-  let(:player) { Player.new(user_interface, 'X') }
-
-  class TestUserInterface
-    def display_message(message)
-      'Displayed: ' + message
-    end
+class TestUserInterface
+  def display_message(message)
+    'Displayed: ' + message
   end
+
+  def display_board(board_state)
+    board_str = board_state.flatten.map { |square| square || 'nil' }
+    'Displayed: ' + board_str.join(',')
+  end
+end
+
+GAME_MESSENGER_MESSAGES = {
+  hello: 'Hello',
+  bye: 'Bye'
+}.freeze
+
+RSpec.describe GameMessenger do
+  let(:board) { Board.new }
+  let(:test_user_interface) { TestUserInterface.new }
+  let(:game_messenger) { GameMessenger.new(user_interface: test_user_interface, messages: GAME_MESSENGER_MESSAGES) }
+  let(:player) { Player.new(user_interface, 'X') }
 
   describe 'display' do
     it 'should display the associated message given a symbol' do
-      messages = {
-        hello: 'Hello',
-        bye: 'Bye'
-      }
-      test_user_interface = TestUserInterface.new
-      game_messenger = GameMessenger.new(user_interface: test_user_interface, messages: messages)
-
       displayed_message = game_messenger.display message: :hello
       expect(displayed_message).to eq('Displayed: Hello')
 
