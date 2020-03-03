6cb36a47df887c2f4cf9df216676d606144d5065
k3ntako <kentarokaneki@gmail.com>
2020-03-03 17:41:39 -0500

    Prompt for move feature test

--- features/step_definitions/board_steps.rb
+++ features/step_definitions/board_steps.rb
@@ -7,7 +7,6 @@ require_relative '../../lib/game_messenger'
 
 original_stdout = $stdout
 
-
 When(/^the game starts$/) do
   $stdout = StringIO.new
 
@@ -26,6 +25,11 @@ Then(/^I should see the empty board$/) do
   expect(stdout_ouput[4]).to eq ' 4 | 5 | 6 '
   expect(stdout_ouput[5]).to eq '-----------'
   expect(stdout_ouput[6]).to eq ' 7 | 8 | 9 '
+end
+
+Then(/^I should be prompted to make a move$/) do
+  stdout_ouput = $stdout.string.split("\n")
+  expect(stdout_ouput[8]).to eq 'Enter a number to make a move in the corresponding square (X\'s turn):'
 
   $stdout = original_stdout
 end
--- features/step_definitions/greeting_steps.rb
+++ features/step_definitions/greeting_steps.rb
@@ -5,6 +5,8 @@ require_relative '../../lib/tic_tac_toe'
 require_relative '../../lib/game_state'
 require_relative '../../lib/game_messenger'
 
+original_stdout = $stdout
+
 When(/^I start the game$/) do
   $stdout = StringIO.new
 
@@ -19,4 +21,6 @@ end
 Then(/^I should see the welcome message$/) do
   stdout_ouput = $stdout.string.split("\n")
   expect(stdout_ouput[0]).to eq 'Welcome to a game of Tic-Tac-Toe!'
+
+  $stdout = original_stdout
 end
